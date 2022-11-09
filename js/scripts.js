b59230c049750a23de7347335f50115a9229a507
Marlon James <marlonjamesdev@gmail.com>
2022-11-09 00:26:40 -0300

    Stage 4 completed

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,12 @@ function createMatch(team1, hour, team2) {
     `
 }
 
+let delay = 0;
+
 function createCard(date, day, match) {
+    delay = delay + 0.8;
     return `
-        <div class="card">
+        <div class="card" style="animation-delay: ${delay}s">
             <h2>${date} <span>${day}</span></h2>
             <ul>
                 ${match}
@@ -19,19 +22,16 @@ function createCard(date, day, match) {
     `
 }
 
-document.querySelector('#app').innerHTML = `
-    <header>
-        <img src="./img/FIFAWC.png" alt="2022 FIFA World Cup logo." />
-    </header>
-    <main id="cards">
-        ${createCard('24 Nov', 'thursday', 
-            createMatch('switzerland', '07:00', 'cameroon') +
-                createMatch('brazil', '16:00', 'serbia'))}
-        ${createCard('28 Nov', 'monday', 
-            createMatch('cameroon', '07:00', 'serbia') +
-                createMatch('brazil', '13:00', 'switzerland'))}
-        ${createCard('02 Dec', 'friday', 
-            createMatch('serbia', '16:00', 'switzerland') +
-                createMatch('cameroon', '16:00', 'brazil'))}
-    </main>
-`
+document.querySelector('#cards').innerHTML =
+    createCard('24 Nov', 'thursday',
+        createMatch('switzerland', '07:00', 'cameroon') +
+        createMatch('brazil', '16:00', 'serbia')
+    ) +
+    createCard('28 Nov', 'monday',
+        createMatch('cameroon', '07:00', 'serbia') +
+        createMatch('brazil', '13:00', 'switzerland')
+    ) +
+    createCard('02 Dec', 'friday',
+        createMatch('serbia', '16:00', 'switzerland') +
+        createMatch('cameroon', '16:00', 'brazil')
+    ) 
